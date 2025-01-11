9254eb0befd91010e76adcffc2d7acba1fc9b8f2
Sanjay Rawat <sanjayrawat0570@gmail.com>
2025-01-11 11:12:28 +0530

    add solution for maximum profit with at most k transactions

---
+++ problem16.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+    int solve(int index,int buy,int cnt,int n,vector<int> &prices){
+        if(index==n){
+            return 0;
+        }
+        if(cnt==0){
+            return 0;
+        }
+        int profit=0;
+        if(buy){
+            int pick=-prices[index]+solve(index+1,false,cnt,n,prices);
+            int notPick=0+solve(index+1,true,cnt,n,prices);
+            profit=max(pick,notPick);
+        }
+        else{
+            int pick=prices[index]+solve(index+1,true,cnt-1,n,prices);
+            int notPick=0+solve(index+1,false,cnt,n,prices);
+            profit=max(pick,notPick);
+        }
+        return profit;
+    }
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        int n=prices.size();
+        return solve(0,true,k,n,prices);
+    }
+};
+int main (){
+    Solution s;
+    vector<int> prices = {2,4,1};
+    cout<<s.maxProfit(2,prices);
+}
